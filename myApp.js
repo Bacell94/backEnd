3c9ab9c01993a20639121b4cd5dc957f9f96c994
Bassel saleh <bacell94@gmail.com>
2022-04-07 18:46:13 +0300

    use assets in  /public

--- myApp.js
+++ myApp.js
@@ -3,8 +3,9 @@ var app = express();
 
 
 console.log("Hello World")
-app.get('/', (seq,res)=> res.sendFile(__dirname + "/views/index.html" ))
-app.get('/', (req,res)=> {res.send('Hello Express')})
+app.get('/', (req,res)=> res.sendFile(__dirname + "/views/index.html" ))
+app.use('/public' ,express.static(__dirname + "/public"))
+// app.get('/', (req,res)=> {res.send('Hello Express')})
 
 
 
