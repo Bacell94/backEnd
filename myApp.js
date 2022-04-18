21aa1da66b2a9d00bd77ef1b146e3e5210851b3e
Bassel saleh <bacell94@gmail.com>
2022-04-18 14:14:48 +0300

    myapp /json fix

--- myApp.js
+++ myApp.js
@@ -5,7 +5,8 @@ var app = express();
 
 console.log("Hello World")
 app.get('/', (req,res)=> res.sendFile(__dirname + "/views/index.html" ))
-app.get('/json', (req,res)=> res.json({"message" :  process.env.MESSAGE_STYLE == 'uppercase' ? "HELLO JSON": "Hello json"}) )
+app.get('/json', (req,res)=> res.json(process.env.MESSAGE_STYLE == 'uppercase' ?
+{"message" : "HELLO JSON"} : {"message" : "Hello json"}) )
 app.use('/public' ,express.static(__dirname + "/public"))
 // app.get('/', (req,res)=> {res.send('Hello Express')})
 
