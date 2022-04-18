8f9903db5852236ffb070142c4c560cb80af5bc5
Bassel saleh <bacell94@gmail.com>
2022-04-18 22:27:15 +0300

    added middlware at my app

--- myApp.js
+++ myApp.js
@@ -1,15 +1,20 @@
 require('dotenv').config()
 var express = require('express');
+const req = require('express/lib/request');
 const { json } = require('express/lib/response');
 var app = express();
 
 
 console.log("Hello World")
+app.use((req,res,next)=> {
+    console.log(`${req.method} ${req.path} ${req.ip}`)
+    next()
+    })
 app.get('/', (req,res)=> res.sendFile(__dirname + "/views/index.html" ))
 app.get('/json', (req,res)=> {
     let msg= {"message" : "Hello json"}
     if(process.env.MESSAGE_STYLE == 'uppercase'){
-        msg = {"message" : "HELLO JSON"}
+        msg.message = "HELLO JSON"
     }
     return res.json(msg)
     })
