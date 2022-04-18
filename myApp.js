e5e208674a916e38882ddc01115557dc0291fc57
Bassel saleh <bacell94@gmail.com>
2022-04-18 16:51:21 +0300

    refactor myapp app.get /json

--- myApp.js
+++ myApp.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 var express = require('express');
+const { json } = require('express/lib/response');
 var app = express();
 
 
 console.log("Hello World")
 app.get('/', (req,res)=> res.sendFile(__dirname + "/views/index.html" ))
-app.get('/json', (req,res)=> res.json(process.env.MESSAGE_STYLE == 'uppercase' ?
-{"message" : "HELLO JSON"} : {"message" : "Hello json"}) )
+app.get('/json', (req,res)=> {
+    let msg= {"message" : "Hello json"}
+    if(process.env.MESSAGE_STYLE == 'uppercase'){
+        msg = {"message" : "HELLO JSON"}
+    }
+    return res.json(msg)
+    })
 app.use('/public' ,express.static(__dirname + "/public"))
 // app.get('/', (req,res)=> {res.send('Hello Express')})
 
